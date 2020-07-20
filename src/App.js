3e2df8cb16295679ed3c6d14c6eefa7b73abced0
italo-lima <italojonas@hotmail.com>
2020-07-20 16:57:15 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store";
+import { store, persistor } from "./store";
 import TodoList from "./TodoList";
 import Counter from "./Counter";
 
 function App() {
   return (
     <Provider store={store}>
-      <TodoList />
-      <Counter />
+      <PersistGate loading={null} persistor={persistor}>
+        <TodoList />
+        <Counter />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import reducers from "./ducks";
 import rootSaga from "./ducks/sagas";
 
+const persistConfig = {
+  key: "@root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
